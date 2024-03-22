8f29d5a86b8b5922ab7a88654d19368f825ef0b6
AbdullahElmeslimany <aelmeslimany24@gmail.com>
2024-03-22 11:05:06 +0200

    Edit Profile Logic

--- lib/view/setting_page/setting_page.dart
+++ lib/view/setting_page/setting_page.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grad_project/model_view/textfromfieldcustom.dart';
-
 import '../../model_view/bloc/profile_cubit/get_data_profile_cubit.dart';
 
 class SettingPage extends StatelessWidget {
